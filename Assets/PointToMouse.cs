6f813eaed36ff9dcf2c4b0323acc1b5739a4d40c
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2025-04-01 23:20:07 -0300

    :update: Placeholder assets implemented

---
+++ Assets/PointToMouse.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PointToMouse : MonoBehaviour
+{
+    void Update()
+    {
+        PointToMousePosition();
+    }
+
+    private void PointToMousePosition()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z; // Mantém a posição Z
+
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
